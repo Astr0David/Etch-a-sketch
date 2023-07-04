7469565fd2260196ba46368b336a98a23053c7cc
David Orimoyegun <119695055+Astr0David@users.noreply.github.com>
2023-07-04 14:55:23 +0100

    Added functionality for user to change grid size

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // Get the grid container
 const gridContainer = document.getElementById('gridContainer');
+const gridButton = document.getElementById('gridSizeButton');
 
 // Specify the number of cells in the grid
-const numCells = 32; // Increase this number to increase the number of cells in the grid
+let numCells = 32; // Increase this number to increase the number of cells in the grid
 
 // Calculate the cell size based on the container size and number of cells
 const containerSize = gridContainer.offsetWidth; // Assumes the container has a fixed width and height
@@ -18,3 +19,34 @@ for (let i = 0; i < numCells * numCells; i++) {
   cell.className = 'cell';
   gridContainer.appendChild(cell);
 }
+
+// Create function for button press (allow user to change grid size)
+gridButton.addEventListener ("click", function() {
+    const gridsize = prompt("Please enter desired grid size (MAX 100):", `${numCells}`);
+  
+    if (gridsize === null) {
+    window.alert("User cancelled prompt.");
+    } else {
+    const gridsize2 = parseInt(gridsize, 10);
+
+    if (isNaN(gridsize2) || gridsize2 < 2 || gridsize2 > 100) {
+        window.alert("Invalid grid size.");
+    } else {
+        numCells = gridsize2;
+
+        const containerSize = gridContainer.offsetWidth;
+        const cellSize = containerSize / numCells;
+
+        gridContainer.innerHTML = '';
+
+        gridContainer.style.gridTemplateColumns = `repeat(${numCells}, ${cellSize}px)`;
+        gridContainer.style.gridTemplateRows = `repeat(${numCells}, ${cellSize}px)`;
+
+        for (let i = 0; i < numCells * numCells; i++) {
+        const cell = document.createElement('div');
+        cell.className = 'cell';
+        gridContainer.appendChild(cell);
+        }}}
+});
+
+
